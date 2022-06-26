1724b9d69610646fed7df452e863047f38e9aeff
Rahat SainiP <rahatsaini@yahoo.com>
2022-06-27 09:19:46 +1000

    added readOnly props

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 var now = moment().format("dddd, MMMM Do YYYY, h:mm a");
 $("#currentDay").text(now);
 
-var events = [
+var events_ = [
   {
     start: "Sun Jun 26 2022 09:00:00",
     end: "Sun Jun 26 2022 09:30:00",
@@ -41,35 +41,70 @@ var events = [
     description: "icecream",
   },
 ];
-
-
+var events = localStorage.getItem(events) ?  localStorage.getItem(events) : events_;
 function assignTask() {
-        events.forEach((x) => {
-            if (moment(x.start).format("DD/MM/YY") == moment().format("DD/MM/YY")) {
-                const startTime = moment(x.start).format("H");
-    const endTime = moment(x.end).format("H");
-    var row = document.getElementsByClassName("col-10");
-    for (var i = 0; i < row.length; i++) {
-      const id = row[i].id;
-      if (id === `event${startTime}`) {
-        document.getElementById(id).innerText = x.description;
-        if (
-          moment(x.start).format("DD/MM/YY HH") < moment().format("DD/MM/YY HH")
-        ) {
-          document.getElementById(id).classList.add("past");
+
+  events.forEach((x) => {
+    if (moment(x.start).format("DD/MM/YY") == moment().format("DD/MM/YY")) {
+      const startTime = moment(x.start).format("H");
+      const endTime = moment(x.end).format("H");
+      var row = document.getElementsByClassName("col-10");
+      for (var i = 0; i < row.length; i++) {
+        const id = row[i].id;
+        if (id === `event${startTime}`) {
+          document.getElementById(id).innerText = x.description;
+          if (
+            moment(x.start).format("DD/MM/YY HH") <
+            moment().format("DD/MM/YY HH")
+          ) {
+            document.getElementById(id).classList.add("past");
+          } else if (
+            moment(x.start).format("DD/MM/YY HH") >
+            moment().format("DD/MM/YY HH")
+          ) {
+            document.getElementById(id).classList.add("future");
+          } else {
+            document.getElementById(id).classList.add("present");
+          }
         }
-        else if (
-                moment(x.start).format("DD/MM/YY HH") > moment().format("DD/MM/YY HH")
-         ) {
-                document.getElementById(id).classList.add("future");}
-         else {       document.getElementById(id).classList.add("present");}
-              
+      }
     }
-}} });
+  });
 }
 
 assignTask();
 
-$('.form-control').click(function(){
-  document.getElementById(this.id).setAttribute('readonly', false);
-  })
+$(".form-control").click(function () {
+  this.readOnly = false;
+});
+
+$(".fa-save").click(function () {
+  const id = `event${this.parentNode.id.replace("save", "")}`;
+  const hour = `${this.parentNode.id.replace("save", "")}`;
+  const text = document.getElementById(id).value;
+  let updated = false;
+  events.forEach((x) => {
+    if (moment(x.start).format("DD/MM/YY") == moment().format("DD/MM/YY")) {
+      const startTime = moment(x.start).format("H");
+      if(hour === startTime)
+      {
+        x.description = text;
+        localStorage.setItem('events', events);
+        updated = true;
+      } 
+
+    }
+  });
+  if(!updated)
+  {
+    const event = {
+      start: new Date(),
+      end: new Date(),
+      isFinished: false,
+      description: text,
+    }
+    events.push(event);
+    localStorage.setItem('events', events);
+  }
+  assignTask();
+});
