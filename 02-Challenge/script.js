562b6aeb49727cad9ce6176d5bfacb1cfdcc3e41
Anu.Saini <105421965+Anu-Saini@users.noreply.github.com>
2022-07-05 23:07:21 +1000

    investigate

--- 02-Challenge/script.js
+++ 02-Challenge/script.js
@@ -16,8 +16,6 @@ var city = nameInputEl.value.trim();
    } else {
      alert('Please enter a City name');
    }
-
-
   var requestedUrl ='https://api.openweathermap.org/data/2.5/forecast?q='+city+'&id=524901&appid=9b1192177659da66737e8df6341c92d7&units=metric'
 
   fetch(requestedUrl)
@@ -26,6 +24,7 @@ var city = nameInputEl.value.trim();
      return    response.json();})
      .then(function (data) {
 box=(data);
+console.log(data);
 let list = document.createElement('ul');
 var temp1 = document.createElement('li');
 var temp_max = document.createElement('li');
@@ -34,34 +33,34 @@ var wind1 = document.createElement('li');
 let pressure1 = document.createElement('li');
 let moist = document.createElement('li');
 
-
 temp1.innerText=`Temp: ${box.list[0].main.temp} ℃`;
+console.log(temp1)
 temp_max.innerText=`Temp: ${box.list[0].main.temp_max} ℃`;
 cloudy.innerText= `sky: ${box.list[0].weather.discription} `;
 wind1.innerText=`Wind: ${box.list[0].wind.speed} km/hr`;
 moist.innerText=`Humidity: ${box.list[0].main.humidity} g/kg`;
 pressure1.innerText=`Pressure: ${box.list[0].main.pressure} kPa`;
-
+console.log(pressure1)
 let head=document.createElement('h3')
 head.innerText= `City : ${box.city.name}`;
 
-// const moment = require('moment');
-// let now = moment();
-// console.log(now.format("L"));
-
 locationCity.append(head);
-
+console.log(locationCity)
 list.append(temp1);
 list.append(temp_max);
 list.append(cloudy);
 list.append(wind1);
 list.append(moist);
 list.append(pressure1);
-
 locationCity.append(list);
-
 });
 }
+
+
+// const moment = require('moment');
+// let now = moment();
+// console.log(now.format("L"));
+
 
 //function being reiterated for the rest of the days
 var formSubmitHandler = function (event) {
